a680e7d5abbefbbe5d31262dfe58cc2530f7036e
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2026-04-02 11:12:40 -0600

    Fix: forzar HTTPS para evitar redirecciones en subida de archivos

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/app.scss',
+                'resources/css/index.scss',
+                'resources/js/app.js',
+                'resources/js/bg.js'
+            ],
             refresh: true,
         }),
     ],
